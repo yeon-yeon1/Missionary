52b8245fd5f0d5985ce203986aa9c4ae6c55e337
yeon-yeon1 <nojk0106@naver.com>
2024-07-01 04:17:48 +0900

    first react

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  arrowPaarens: 'avoid',
+  semi: false,
+  printWidth: 90,
+};
---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+export default function App() {
+  console.log('App called.')
+  return <h1>Hello world!</h1>
+}
